a6600937396fe8cf5b846d3c8a76ef9c18b62f64
Julian Ganguli <julianganguli@gmail.com>
2014-12-21 14:06:27 -0800

    commit ui and server files

---
+++ server.R
@@ -0,0 +1,36 @@
+library('shiny')
+library('dplyr')
+library('ggplot2')
+library('reshape2')
+load('global_stats.Rda')
+global.stats$year <- as.numeric(global.stats$year)
+names(global.stats) <- tolower(names(global.stats))
+global.stats <- select(global.stats, country.code, country.name, year, population, internet_users)
+global.stats <- melt(global.stats, id.vars = c('year', 'country.code', 'country.name'))
+
+# define server logic required to draw a line chart
+shinyServer(function(input, output) {
+    
+#     country.code.data <- reactive(global.stats %>% filter(country.code == input$countryCode))
+    country.code.data <- reactive(
+        global.stats[which(global.stats$country.code == input$countryCode),]
+        )
+
+    output$countryPlot <- renderPlot({
+        ggplot(country.code.data(), aes(x = year, y = value, group = variable, color = variable)) +
+            geom_point() +
+            geom_line() +
+            theme_bw() +
+            theme(legend.justification=c(1,0),
+                  legend.position=c(1,0),
+                  legend.title = element_blank(),
+                  axis.title.x = element_text(size = 16),
+                  axis.title.y = element_text(size = 16),
+                  plot.title = element_text(size = 18)) +
+            ggtitle(paste0('Population and internet users for: ', input$countryCode)) +
+            ylab('') +
+            xlim(input$year[1], input$year[2])
+        })
+    }
+)
+
---
+++ ui.R
@@ -0,0 +1,28 @@
+library('shiny')
+
+# define ui for application that plots population and internet usage by country over time.
+shinyUI(pageWithSidebar(
+    
+    # application title
+    headerPanel("Global Population and Internet Usage by Country"),
+    
+    # sidebar with a slider input for time series
+    sidebarPanel(
+        sliderInput("year", "Select years to display:", min=1965,
+                    max=2013, value=c(1965, 2013), format="####"),
+        
+        # input country code
+        textInput("countryCode", label="Enter the country code (upper case)", value = "USA"),
+        
+        # about the app
+        h3('About this app'),
+        p('This application plots the population and number of people with internet access for a given country. All data was provided by the', a('World Bank.', href = 'http://www.worldbank.org/'), 'You can find a complete list of country codes at ', a('CountryCode.org', href = 'http://countrycode.org/')),
+        h3('Development'),
+        p('Created by ', a('Julian Ganguli', href = 'https://www.linkedin.com/in/julianganguli'),' using ', a('Shiny', href = 'http://shiny.rstudio.com/'), ' and hosted on ', a('ShinyApps.io', href= 'https://www.shinyapps.io'))
+    ),
+    
+    mainPanel(
+        
+        plotOutput('countryPlot')
+    )
+))
